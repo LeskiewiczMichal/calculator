a9073a4541940a4b3e1e681cb216f5f32cd1c1f1
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-02-28 21:58:29 +0100

    Screen displaying properly

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// function curry(f) {
-//     return function(num1) {
-//         return function(operator) {
-//             return function(num2){
-//                 return f(num1, operator, num2);
-//             }
-//         }
-//     }
-// }
-
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -30,12 +20,6 @@ function clear() {
     this.operator = undefined;
 }
 
-
-
-
-
-
-
 function operate(num1, num2, operator) {
     let result = 0;
     switch(operator) {
@@ -55,55 +39,21 @@ function operate(num1, num2, operator) {
     return result;
 }
 
+const keys = document.querySelector('.calculator_keys');
+const screen = document.querySelector('#screen');
 
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')){
+        const keyContent = e.target.textContent;
+        const usedFor = e.target.dataset.usedFor;
+        const display = screen.textContent;
 
-// Function to get clicked button content
-// for (let i = 0; i < numButtons.length; i++) {
-    // numButtons[i].addEventListener('click', getButtonContent);
-    // buttons[i].addEventListener('click', changeScreen);
-// 
-// }
-
-function getButtonContent() {
-    return event.target.firstChild.nodeValue ;
-}
-
-
-
-// function changeScreen() {
-//     let contentForScreen = getButtonContent();
-//     let num1;
-//     let num2;
-//     let operator;
-//     // console.log(contentForScreen)
-//     // screen.textContent = event.target.textContent;
-//     // screen.textContent = getButtonContent();
-//     switch (contentForScreen) {
-//         case '1':
-//         case '2':
-//         case '3':
-//         case '4':
-//         case '5':
-//         case '6':
-//         case '7':
-//         case '8':
-//         case '9':
-//         case '0':
-//         case '.':
-//             screen.textContent += contentForScreen;
-//             numbersForOperation += contentForScreen;
-//             break;
-//         case 'AC':
-//             screen.textContent = '';
-//         case '+':
-//             num1 = Number(numbersForOperation)
-//             numbersForOperation = '';
-//             screen.textContent = '';
-//             console.log(num1)
-//             operator = '+'
-//         case '=':
-//             num2 = Number(numbersForOperation)
-//             console.log(num2)
-//             console.log(operate(num1, num2, operator))
-//     }
-// }
+        if (!usedFor) {
+            if (display === '0') {
+                screen.textContent = keyContent;
+            } else {
+                screen.textContent = display + keyContent;
+            }
+        }
+    }
+})
