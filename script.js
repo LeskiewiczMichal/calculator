57626189b630a3a3e69cea333a7cb60d0cba251e
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-02-28 21:46:09 +0100

    Changing how the calculator is built WIP

--- script.js
+++ script.js
@@ -24,8 +24,19 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function clear() {
+    this.num1 = '';
+    this.num2 = '';
+    this.operator = undefined;
+}
 
-function operator(num1, num2, operator) {
+
+
+
+
+
+
+function operate(num1, num2, operator) {
     let result = 0;
     switch(operator) {
         case '+':
@@ -44,39 +55,55 @@ function operator(num1, num2, operator) {
     return result;
 }
 
-let buttons = document.querySelectorAll('.button');
+
 
 // Function to get clicked button content
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', getButtonContent);
-    buttons[i].addEventListener('click', changeScreen);
-
-}
+// for (let i = 0; i < numButtons.length; i++) {
+    // numButtons[i].addEventListener('click', getButtonContent);
+    // buttons[i].addEventListener('click', changeScreen);
+// 
+// }
 
 function getButtonContent() {
     return event.target.firstChild.nodeValue ;
 }
 
-let screen = document.getElementById('screen');
 
-function changeScreen() {
-    let contentForScreen = getButtonContent();
-    // console.log(contentForScreen)
-    // screen.textContent = event.target.textContent;
-    // screen.textContent = getButtonContent();
-    switch (contentForScreen) {
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-        case '0':
-        case '.':
-            screen.textContent += contentForScreen;
-            break;
-    }
-}
+
+// function changeScreen() {
+//     let contentForScreen = getButtonContent();
+//     let num1;
+//     let num2;
+//     let operator;
+//     // console.log(contentForScreen)
+//     // screen.textContent = event.target.textContent;
+//     // screen.textContent = getButtonContent();
+//     switch (contentForScreen) {
+//         case '1':
+//         case '2':
+//         case '3':
+//         case '4':
+//         case '5':
+//         case '6':
+//         case '7':
+//         case '8':
+//         case '9':
+//         case '0':
+//         case '.':
+//             screen.textContent += contentForScreen;
+//             numbersForOperation += contentForScreen;
+//             break;
+//         case 'AC':
+//             screen.textContent = '';
+//         case '+':
+//             num1 = Number(numbersForOperation)
+//             numbersForOperation = '';
+//             screen.textContent = '';
+//             console.log(num1)
+//             operator = '+'
+//         case '=':
+//             num2 = Number(numbersForOperation)
+//             console.log(num2)
+//             console.log(operate(num1, num2, operator))
+//     }
+// }
