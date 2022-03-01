c06369a5420a85054e2a5be59012a4d7296c179e
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-03-01 15:34:58 +0100

    Functionality -ac, decimal and bugfixes

--- script.js
+++ script.js
@@ -11,18 +11,17 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function clear() {
-    this.num1 = '';
-    this.num2 = '';
-    this.operator = undefined;
+    if (num2 === 0) {
+        return 'Nope';
+    } else {
+        return num1 / num2;
+    }
 }
 
 function operate(number1, number2, operator) {
     const num1 = parseFloat(number1);
     const num2 = parseFloat(number2);
+    
     let result = 0;
     switch(operator) {
         case '+':
@@ -52,11 +51,20 @@ keys.addEventListener('click', e => {
         const previousKey = calculator.dataset.previousKey;
 
         if (!usedFor) {
-            if (display === '0' || previousKey === 'operator') {
+            if (display === 'Enter valid numbers') {
+                calculator.dataset.firstNumber = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKey = '';
+                screen.textContent = keyContent;
+            } else if (
+                display === '0' ||
+                previousKey === 'operator'
+                ) {
                 screen.textContent = keyContent;
             } else {
                 screen.textContent = display + keyContent;
             }
+            calculator.dataset.previousKey = 'number';
         };
 
         if (
@@ -65,7 +73,30 @@ keys.addEventListener('click', e => {
             usedFor === 'divide' ||
             usedFor === 'multiply'
         ) {
-            calculator.dataset.firstNumber = display;
+            let firstNumber = calculator.dataset.firstNumber;
+            const operator = calculator.dataset.operator;
+            const secondNumber = display;
+
+            if (
+                firstNumber &&
+                operator &&
+                previousKey !== 'calculate'
+            ) {
+                let result = operate(firstNumber, secondNumber, operator);
+                if (isNaN(result)) {
+                    screen.style.fontSize = '50px';
+                    result = 'Enter valid numbers';
+                } else if (result.toString().length > 11) {
+                    screen.style.fontSize = '50px';
+                    result = 'Number too big.'
+                }
+                screen.textContent = result;
+                calculator.dataset.firstNumber = result;
+            } else {
+                calculator.dataset.firstNumber = display;
+            }
+
+
             calculator.dataset.operator = keyContent;
             calculator.dataset.previousKey = 'operator';
 
@@ -75,11 +106,32 @@ keys.addEventListener('click', e => {
             const firstNumber = calculator.dataset.firstNumber;
             const secondNumber = display;
             const operator = calculator.dataset.operator;
+            calculator.dataset.previousKey = 'calculate';
             console.log(firstNumber)
             console.log(secondNumber)
-            const result = operate(firstNumber, secondNumber, operator)
-
+            let result = operate(firstNumber, secondNumber, operator);
+            if (isNaN(result)) {
+                screen.style.fontSize = '50px';
+                result = 'Enter valid numbers';
+            } else if (result.toString().length > 11) {
+                screen.style.fontSize = '50px';
+                result = 'Number too big.'
+            }
             screen.textContent = result;
+        };
+
+        if (usedFor === 'clear') {
+            screen.textContent = 0;
+            calculator.dataset.firstNumber = '';
+            calculator.dataset.operator = '';
+            calculator.dataset.previousKey = '';
+        };
+
+        if (usedFor === 'decimal') {
+            if (!display.includes('.')) {
+                screen.textContent = display + '.';
+            };
+            calculator.dataset.previousKey = 'decimal'
         }
     }
 })
